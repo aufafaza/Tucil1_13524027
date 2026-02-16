04e9eb3a6ba51fa226e7ff8e9dea08f51162e215
aufafaza <aufa.rienaldifaza.a@gmail.com>
2026-02-16 20:06:21 +0700

    board model and test file

---
+++ src/models/board.go
@@ -0,0 +1,16 @@
+package models
+
+type Board struct {
+	Size      int
+	Grid      [][]string
+	Q         []int
+	Solutions int
+}
+
+func NewBoard(size int, gridData [][]string) *Board {
+	return &Board{
+		Size: size,
+		Grid: gridData,
+		Q:    make([]int, size), // jumlah Q = n (n x n grid)
+	}
+}
